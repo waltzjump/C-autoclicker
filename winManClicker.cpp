a1c719656051bd5dd7f266634b65406c8f896a1d
waltzjump <101656710+waltzjump@users.noreply.github.com>
2026-05-11 19:32:02 -0600

    got clicking functionality on timer windows clicker

--- winManClicker.cpp
+++ winManClicker.cpp
@@ -1,9 +1,35 @@
 #include "winManClicker.h"
+#include <winuser.h>
+#include <strsafe.h>
 
 void WinManClicker::click(int cps) {
-    ;
+    
+
 }
 
 void WinManClicker::runUntil() {
-    ;
+    /*
+        runUntil for the windows manual clicker will run until the end user presses the spacebar
+        uses Windowhooks to handle the input monitoring
+    */
+
+    bool done = false;
+    while (!done) {
+
+    }
+    
 }
+
+
+/*
+typedef struct _MYHOOKDATA 
+{ 
+    int nType; 
+    HOOKPROC hkprc; 
+    HHOOK hhook; 
+} MYHOOKDATA; 
+ 
+MYHOOKDATA myhookdata[NUMHOOKS]; 
+
+HWND gh_hwndMain;
+*/
